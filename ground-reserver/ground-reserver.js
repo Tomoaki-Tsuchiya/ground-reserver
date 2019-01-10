a072762947b3d9d5c19140d30a794ee3e75226c0
Tomoaki-Tsuchiya <tomt0m.25.g00cl@gmail.com>
2019-01-10 19:55:41 +0900

    completing redirect but it fails

--- ground-reserver/ground-reserver.js
+++ ground-reserver/ground-reserver.js
@@ -5,14 +5,14 @@ const _ = require('lodash');
 
 var cookiejar = rpn.jar();
 
-var onStatus302 = function(base_uri, redir_uri, jsessionid) {
+var onStatus302 = function(base_uri, redir_uri) {
     var unique_key_array = _.split(redir_uri, '?');
     var unique_key = unique_key_array[1];
 
-    var sessionid_array = _.split(jsessionid,/=\;/);
-    var sessionid = sessionid_array[2];
+    var sessionid_array = _.split(unique_key_array[0],';');
+    var sessionid = sessionid_array[1];
 
-    var new_uri = base_uri + ';jsessionid=' + sessionid + '?' + unique_key;
+    var new_uri = base_uri /** + ';' + sessionid */ + '?' + unique_key;
     return new_uri;
 };
 
@@ -28,22 +28,27 @@ rpn(options).then((result) => {
     console.log('Access Success!! ', result);
     console.log('StatusCode: ', result.statusCode);
     console.log('Cookie: ', cookiejar);
+
+    // return new Promise.resolve(result);
 }, (error) => {
     console.log('Error!; ', error);
     console.log('Cookie: ', typeof(cookiejar));
     console.log('redir_url: ', error.response.headers.location);
-    
-    var jsessionid = cookiejar["JSESSIONID"];
 
-    console.log('sessionid:', jsessionid);
-
-    var newUri = onStatus302(options.uri, error.response.headers.location, jsessionid);
+    var newUri = onStatus302(options.uri, error.response.headers.location);
     console.log('new_uri: ', newUri);
-    // var options = {
-    //     method: 'POST',
-    //     // uri: 'https://sports.tef.or.jp/user/view/user/homeIndex.html',
-    //     uri: 'https://sports.tef.or.jp/user/view/user/mypIndex.html',
-    //     resolveWithFullResponse: true,
-    //     jar: cookiejar
-    // }
-})
+    var opts = {
+        method: 'POST',
+        // uri: 'https://sports.tef.or.jp/user/view/user/homeIndex.html',
+        uri: newUri,
+        resolveWithFullResponse: true,
+        jar: cookiejar
+    }
+    return rpn(opts)
+// });
+}).then((result) => {
+    console.log('Redirect Success!!');
+}, (err) => {
+    console.log('status; ', err.statusCode);
+    console.log('Redirect Error!!')
+});
